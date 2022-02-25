0628f9939aeb8ec9262bfd735c002725446e7df0
Eric Anastas <eric.anastas@som.com>
2022-02-25 03:14:09 -0800

    Changed to .env file

--- start.ps1
+++ start.ps1
@@ -1,3 +1,3 @@
 docker rm node-app -f
 docker build -t node-app-image .
-docker run --name node-app -d -p 3000:4000 -v ${pwd}:/app:ro -v /app/node_modules --env PORT=4000  node-app-image
+docker run --name node-app -d -p 3000:4000 -v ${pwd}:/app:ro -v /app/node_modules --env-file ./.env node-app-image
