b6b6790724f450eee550ee1601682529d372e05c
Eric Anastas <eric.anastas@som.com>
2022-02-28 22:35:19 -0800

    Split into seperate dev and prod docker-compose files

--- start.ps1
+++ start.ps1
@@ -1,1 +1,1 @@
-docker-compose up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
--- stop.ps1
+++ stop.ps1
@@ -1,1 +1,1 @@
-docker-compose down -v
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml down -v
