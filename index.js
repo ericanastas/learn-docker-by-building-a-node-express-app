ef1aa277fa9eecd9045017fd5e9c0478263e5d6b
Eric Anastas <eric.anastas@som.com>
2022-03-01 01:42:24 -0800

    Setup connection to mongo

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+
+mongoose.connect("mongodb://root:mypass@mongo:27017/?authSource=admin")
+    .then(() => console.log("Connected to mongo database"))
+    .catch((e) => console.log(e));
+
+
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
