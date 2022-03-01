43579a6a5866e3340330b4d06a79fa820d3b6dc5
Eric Anastas <eric.anastas@som.com>
2022-03-01 00:59:03 -0800

    Added mongodb named volume

--- stop.dev.ps1
+++ stop.dev.ps1
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml down -v
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml down
--- stop.ps1
+++ stop.ps1
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml down -v
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
