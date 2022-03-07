b16a36244c4034674cb216b55ba3bc8f98241c72
Eric Anastas <eric.anastas@som.com>
2022-03-07 03:22:52 -0800

    Added user to session durring signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,8 @@ exports.signUp = async (req, res) => {
         const hashpassword = await bcrypt.hash(password, 12);
         const newUser = await User.create({ username, password: hashpassword });
 
+
+        req.session.user = newUser;
         res.status(201).json({
             status: "success",
             data: {
