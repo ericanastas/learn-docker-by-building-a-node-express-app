77ece5c1136eb1f988b764f5fe4e3c0cd025c4df
Eric Anastas <eric.anastas@som.com>
2022-03-07 03:23:23 -0800

    Added protect middleware to prevent unauthorized use

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+const protect = (req, res, next) => {
+
+    const { user } = req.session;
+
+    if (!user) {
+        return res.status(401).json({ status: "fail", message: "unauthorized" });
+    }
+
+    req.user = user;
+
+    next();
+}
+
+
+module.exports = protect;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const postController = require("../controllers/postController");
 const router = express.Router();
 
+const protect = require("../middleware/authMiddleware");
+
 
 //localhost:3000/
-router.route("/").get(postController.getAllPosts).post(postController.createPost);
+router.route("/").get(postController.getAllPosts).post(protect, postController.createPost);
 
 //
 router.route("/:id")
-    .get(postController.getOnePost)
-    .patch(postController.updatePost)
-    .delete(postController.deletePost);
+    .get(protect, postController.getOnePost)
+    .patch(protect, postController.updatePost)
+    .delete(protect, postController.deletePost);
 
 module.exports = router;
